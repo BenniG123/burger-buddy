1205aec5900d261b2a9647d739589967ae2e54a7
jay27894 <jay27894@gmail.com>
2017-04-20 23:33:34 -0500

    OrderWindow.cpp
    In progress

---
+++ OrderWindow.cpp
@@ -0,0 +1,30 @@
+#include <systemc.h>
+#include "double_handshake.h"
+#include "OrderWindow.h"
+using namespace std;
+
+void OrderWindow::main() {
+            Meal fromInputWrapper;
+		        MoneyTransaction fromMoneyManager;
+
+            // Outputs
+            Meal toMealCombiner;
+            MoneyTransaction toMoneyManager;
+                
+            while(1)
+            { 
+                      cout << "Waiting for input wrapper" << endl;
+                      
+                      if ()
+            }
+
+
+
+
+
+}
+
+
+
+
+
